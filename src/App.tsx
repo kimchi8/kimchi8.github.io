756c3bbd2af70ac021f11073405b7b152534c281
kimchi <kimmygao808@gmail.com>
2022-12-23 17:39:04 +1000

    reinit react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
 import React from "react";
-import NavBar from "./components/NavBar";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Projects from "./pages/Projects";
+import "./App.css";
+
 function App() {
-    return (
-        <>
-            <Router>
-                <Routes>
-                    <Route path="/home" element={<Home />}></Route>
-                    <Route path="/about" element={<About />}></Route>
-                    <Route path="/work" element={<Projects />}></Route>
-                </Routes>
-            </Router>
-        </>
-    );
+    return <div className="App"></div>;
 }
 
 export default App;
