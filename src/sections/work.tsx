82dd0162e519df6a35570e0fa99d57b0ecd29649
kimchi <kimmygao808@gmail.com>
2023-01-08 22:15:03 +1000

    fixed sticky scrolling of work title and added example image for work profile

--- src/sections/work.tsx
+++ src/sections/work.tsx
@@ -24,39 +24,35 @@ function Work() {
     return (
         <>
             <div className="mt-[30vh] ml-40">
-                <div id="imgBx" className="flex ml-28 ">
-                    <div id="work-title-container" className="h-[180vh] bg-transparent ">
-                        <div id="blank-audera-text" className="sticky top-10 bg-transparent ml-16">
-                            <div className=" w-10 bg-transparent tracking-widest">
-                                <WorkText className="bg-transparent w-48 " />
+                <div>
+                    <div id="imgBx" className="flex ml-40">
+                        <div id="work-title-container" className="h-[125vh] bg-transparent ">
+                            <div id="blank-audera-text" className="sticky top-[11.5rem] bg-transparent ml-16">
+                                <div className=" w-10 bg-transparent tracking-widest">
+                                    <WorkText className="bg-transparent w-48 " />
+                                </div>
                             </div>
-                        </div>
-                        <div className="sticky top-10 bg-transparent ml-16" id="full-audera-text">
-                            <div className="my-[100vh] w-10 bg-transparent text-6xl tracking-widest">
-                                <WorkBlank className="bg-transparent w-48 ml-1" />
+                            <div className="sticky top-10 bg-transparent ml-16" id="full-audera-text">
+                                <div className="my-[100vh] w-10 bg-transparent text-6xl tracking-widest">
+                                    <WorkBlank className="bg-transparent w-48 ml-1" />
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <motion.div
+                        id="about"
+                        className="flex w-screen bg-transparent ml-2 "
+                        variants={boxVariant}
+                        initial="hidden"
+                        animate={control}
+                        ref={ref}
+                    >
+                        <div className="flex w-[60vw] flex-row bg-transparent justify-between">
+                            <img src={audeara} className="w-[30vw] ml-0" />
+                            <p>some text</p>
+                        </div>
+                    </motion.div>
                 </div>
-                <div> </div>
-                <motion.div
-                    id="about"
-                    className="flex relative top-[-36vh] left-[-1.5vw] ml-[16vw] w-4/6 bg-transparent"
-                    variants={boxVariant}
-                    initial="hidden"
-                    animate={control}
-                    ref={ref}
-                >
-                    <div className="flex flex-row gap-[15vw] bg-transparent">
-                        {/* <img
-                            alt="work experience container box"
-                            src={workBox}
-                            className="shadow-[0_25px_50px_-12px_#BFC5E8] rounded-3xl w-4/12 fixed z-0 top-20 left-40 "
-                        /> */}
-                        <img src={audeara} className="w-full" />
-                        <p>some text</p>
-                    </div>
-                </motion.div>
             </div>
         </>
     );
