ed82b15731de2df306285aa7268544be12d9dd4b
kimchi <kimmygao808@gmail.com>
2023-01-11 20:50:03 +1000

    removed brower dom and changed privacy

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import BrowserRouter from "react-router-dom/BrowserRouter";
+// import BrowserRouter from "react-router-dom/BrowserRouter";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
     <React.StrictMode>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-            <App />
-        </BrowserRouter>
+        {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+        <App />
+        {/* </BrowserRouter> */}
     </React.StrictMode>
 );
 
